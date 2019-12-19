fac5cf306e6dc8cfd90845391bb55a142a3e9f3b
djh <dingjinhong123@qq.com>
2019-12-19 15:03:56 +0800

    init

---
+++ kc.tool.axon.ide/src/kc/tool/axon/ide/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,22 @@
+package kc.tool.axon.ide;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "kc.tool.axon.ide.perspective";
+
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+	
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
